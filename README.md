bbd5d9e834d65cab4339f947a5de59f0539122cd
Philip Borndalen <phibor13@gmail.com>
2020-02-05 21:13:36 +0000

    Add Gradle

---
+++ README.md
@@ -0,0 +1,9 @@
+# Property Tycoon
+
+Property Tycoon is a game, develop for in a software engineering course.
+
+The game is a board game like _Monopoly_.
+
+## License
+ 
+MIT
---
+++ src/main/java/com/watson/propert/tycoon/App.java
@@ -0,0 +1,58 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Philip Borndalen
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.watson.propert.tycoon;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App extends Application {
+  public String getGreeting() {
+    return "Hello world.";
+  }
+
+  @Override
+  public void start(Stage stage) throws Exception {
+    String javaVersion = System.getProperty("java.version");
+    String javafxVersion = System.getProperty("javafx.version");
+    Label l =
+        new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+    Scene scene = new Scene(new StackPane(l), 640, 480);
+
+    stage.setTitle("Testing JavaFX");
+    stage.setScene(scene);
+    stage.show();
+  }
+
+  public static void main(String[] args) {
+    Logger logger = LoggerFactory.getLogger(App.class);
+    logger.debug("Hello world.");
+    launch(args);
+  }
+}
