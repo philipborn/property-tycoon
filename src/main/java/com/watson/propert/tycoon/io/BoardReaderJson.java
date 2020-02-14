cf2f148bacada6132e80aa025eae0eed90a86aa2
tomdoran24 <tomdoran20@gmail.com>
2020-02-14 13:40:44 +0000

    1st implementation of the BoardReaderJson class.

--- src/main/java/com/watson/propert/tycoon/io/BoardReaderJson.java
+++ src/main/java/com/watson/propert/tycoon/io/BoardReaderJson.java
@@ -1,27 +1,35 @@
-/*
- * MIT License
- *
- * Copyright (c) 2020 Tom Doran
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- *copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
+//
+//* MIT License
+//*
+//* Copyright (c) 2020 Tom Doran
+//*
+//* Permission is hereby granted, free of charge, to any person obtaining a copy
+//* of this software and associated documentation files (the "Software"), to deal
+//* in the Software without restriction, including without limitation the rights
+//* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//*copies of the Software, and to permit persons to whom the Software is
+//* furnished to do so, subject to the following conditions:
+//*
+//* The above copyright notice and this permission notice shall be included in all
+//* copies or substantial portions of the Software.
+//*
+//* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//* SOFTWARE.
+//*
+//
 package com.watson.propert.tycoon.io;
+/*********************************************************************************
+ Class for extracting objects from a JSON file & returning them as HashMaps for
+ the Board Builder class to use.
+ @author Tom Doran
+ @version 1.0
+ @since 13/02/2020
+ ********************************************************************************/
 
 import org.json.simple.*;
 import org.json.simple.parser.JSONParser;
@@ -35,6 +43,10 @@ public class BoardReaderJson {
     private String[] keys = {"Position", "Name", "Group", "Action", "canBeBought",
             "Cost", "Rent", "1house", "2houses", "3houses", "4houses", "hotel"};
 
+    /**************************************************************************
+     * Constructor for building a Board reader object.
+     * @param fileName path of the JSON file
+     *************************************************************************/
     public BoardReaderJson(String fileName) {
 
         JSONParser jp = new JSONParser();
@@ -50,15 +62,25 @@ public class BoardReaderJson {
         }
     }
 
-    private int dataSize(){
-        return objects.size();
+    /**************************************************************************
+     * Method for iterating through the JSON Array, also checks that the
+     * iterator is within range.
+     * @return boolean true if iterator in range, false if not
+     *************************************************************************/
+    public boolean iterate() {
+        if(iterator < objects.size()-1) {
+            iterator++;
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    private void getNext() {
-        iterator++;
-    }
-
-    private HashMap<String, Object> getObjectData() {
+    /***************************************************************************
+     * Primary funtionality of the class, translates a JSON object to a HashMap
+     * @return HashMap representing the data of a JSON object
+     ***************************************************************************/
+    public HashMap<String, Object> getObjectData() {
 
         HashMap<String,Object> data = new HashMap<>();
         // get current JSONObject
@@ -81,19 +103,11 @@ public class BoardReaderJson {
     public static void main(String[] args) {
 
         // update to read from resource folder
-        BoardReaderJson r = new BoardReaderJson("C:/Users/User/Downloads/boardDataJSON.json");
+        BoardReaderJson r = new BoardReaderJson("src/main/resources/boardDataJSON.json");
 
-        HashMap<String,Object> data = r.getObjectData();
-
-        System.out.println(data.get("Name"));
-        System.out.println(data.get("Cost"));
-        r.getNext();
-        data = r.getObjectData();
-
-        System.out.println(data.get("Name"));
-        r.getNext();
-        data = r.getObjectData();
-        System.out.println(data.get("Name"));
-        System.out.println(data.keySet());
+        for(int i = 0; i < 100; i++) {
+            System.out.println(r.iterate());
+        }
+        System.out.println(r.getObjectData().get("Position"));
     }
 }
---
+++ src/test/java/com/watson/propert/tycoon/gui/BoardReaderJsonTest.java
@@ -0,0 +1,57 @@
+package com.watson.propert.tycoon.gui;
+import com.watson.propert.tycoon.io.BoardReaderJson;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoardReaderJsonTest {
+
+    // board data used for the test
+    BoardReaderJson r = new BoardReaderJson("src/main/resources/boardDataJSON.json");
+
+    @Test
+    // test that output of the board reader class is not null
+    void test_output_not_null() {
+        assertNotNull(r.getObjectData());
+    }
+
+    @Test
+    // test that iterate method indeed moves through the list
+    void test_iterate() {
+        assertEquals(r.getObjectData().get("Position"), 1);
+        r.iterate();
+        assertEquals(r.getObjectData().get("Position"), 2);
+    }
+
+    @Test
+    // test that iterate method does not go over the end of the list
+    void test_iterate_bound() {
+        // 40 squares on board
+        for(int i = 0; i <= 100; i++){
+            r.iterate();
+        }
+        assertEquals(r.getObjectData().get("Position"), 40);
+    }
+
+    @Test
+    // test that Go HashMap doesn't have fields for rent, houses etc.
+    void test_HashMapSize_1() {
+        assertFalse(r.getObjectData().containsKey("Rent"));
+        assertFalse(r.getObjectData().containsKey("Cost"));
+        assertFalse(r.getObjectData().containsKey("1house"));
+        assertFalse(r.getObjectData().containsKey("2houses"));
+        assertFalse(r.getObjectData().containsKey("3houses"));
+        assertFalse(r.getObjectData().containsKey("4houses"));
+        assertFalse(r.getObjectData().containsKey("hotel"));
+    }
+
+    @Test
+    // test that Crapper Street HashMap doesn't have key for action, but does have a key for everything else
+    void test_HashMapSize_2() {
+        r.iterate();
+        assertFalse(r.getObjectData().containsKey("Action"));
+        assertEquals(r.getObjectData().keySet().size(), 11);
+    }
+}
