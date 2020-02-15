496eaf0c8f9aebe75ee8de051b0f0886bc0cb36d
Philip Borndalen <phibor13@gmail.com>
2020-02-15 19:27:14 +0000

    Add randomenes to Dice

--- src/main/java/com/watson/propert/tycoon/game/DicePair.java
+++ src/main/java/com/watson/propert/tycoon/game/DicePair.java
@@ -3,19 +3,29 @@ package com.watson.propert.tycoon.game;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Random;
 
 public class DicePair {
 
+  private Random genrator;
   private int firstDice = 0;
   private int secondDice = 0;
 
+  public DicePair() {
+    genrator = new Random();
+  }
+
   Optional<List<Integer>> throwDices() {
+    firstDice = randomInt();
+    secondDice = randomInt();
     List<Integer> list = new ArrayList<>();
-    list.add(2);
-    list.add(1);
-    firstDice = 2;
-    secondDice = 1;
+    list.add(firstDice);
+    list.add(secondDice);
     return Optional.of(list);
+  }
+
+  private Integer randomInt() {
+    return genrator.nextInt(6) + 1;
   }
 
   Optional<List<Integer>> lastThrow() {
