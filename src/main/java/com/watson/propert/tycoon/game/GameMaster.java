9d151afeddcff090727f48dbff19373ea3ccb3e1
Philip Borndalen <phibor13@gmail.com>
2020-02-19 10:42:21 +0000

    Remove old interface
    Part of making all views only received info by events.

--- src/main/java/com/watson/propert/tycoon/game/GameMaster.java
+++ src/main/java/com/watson/propert/tycoon/game/GameMaster.java
@@ -21,40 +21,9 @@ public class GameMaster implements PropertTycoon {
   }
 
   @Override
-  public List<Integer> throwDices() {
+  public void throwDicesAndMove() {
     List<Integer> dices = dicePair.throwDices();
     Integer sum = dices.stream().mapToInt((a) -> a).sum();
     token.move(sum);
-    return dices;
-  }
-
-  @Override
-  public Optional<List<Integer>> getDices() {
-    return dicePair.lastThrow();
-  }
-
-  @Override
-  public List<String> getSquaresNames() {
-    List<String> names = new ArrayList<>(40);
-    for (Square s : bord) {
-      names.add(s.name());
-    }
-    return names;
-  }
-
-  @Override
-  public int GetPlayerPostion() {
-    return findNumOf(token.postion());
-  }
-
-  private int findNumOf(Square square) {
-    int post = 0;
-    for (Square s : bord) {
-      if (square.name().equals(s.name())) {
-        break;
-      }
-      ++post;
-    }
-    return post;
   }
 }
--- src/main/java/com/watson/propert/tycoon/game/PropertTycoon.java
+++ src/main/java/com/watson/propert/tycoon/game/PropertTycoon.java
@@ -7,31 +7,9 @@ import java.util.Optional;
 public interface PropertTycoon {
 
   /**
-   * Thrown the dices and moves the current player
+   * Thrown the dices and moves the current player.
+   * The value of Dices will received by a DiceEvent
    *
-   * @return Dice pairs value
    */
-  List<Integer> throwDices();
-
-  /**
-   * Return last dice throw if exist.
-   *
-   * @return Dice pare last value
-   */
-  Optional<List<Integer>> getDices();
-
-  /**
-   * Get all squares names in the bord
-   *
-   * @return List with index is same order the square are from go. First square names is at index 0
-   *     and second at index 1.
-   */
-  List<String> getSquaresNames();
-
-  /**
-   * Get the player position. Example: int = 0 is first square, int = 1 is the second square.
-   *
-   * @return position as the index of the square.
-   */
-  int GetPlayerPostion();
+  void throwDicesAndMove();
 }
