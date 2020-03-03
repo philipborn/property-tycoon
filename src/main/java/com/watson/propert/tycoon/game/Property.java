7ee933104910f768ed72fb4355be1e963a4d9d6e
rr-del <60930092+rr-del@users.noreply.github.com>
2020-03-03 16:29:03 +0000

    Property class

---
+++ src/main/java/com/watson/propert/tycoon/game/Property.java
@@ -0,0 +1,42 @@
+package com.watson.propert.tycoon.game;
+
+public abstract class Property {
+
+  private int value;
+  private Player owner;
+  private boolean mortgaged;
+
+  public Property(int amount) {
+    mortgaged = false;
+    value = amount;
+    owner = null;
+  }
+
+  public abstract int getRent();
+
+  public Player owner() {
+    return owner;
+  }
+
+  // value is the property cost not rent
+  public int value() {
+    return value;
+  }
+
+  public void mortgage() {
+    mortgaged = true;
+  }
+
+  public boolean isMortgage() {
+    return mortgaged;
+  }
+
+  public int sell() {
+    owner = null;
+    return value;
+  }
+
+  public void buy(Player player) {
+    owner = player;
+  }
+}
