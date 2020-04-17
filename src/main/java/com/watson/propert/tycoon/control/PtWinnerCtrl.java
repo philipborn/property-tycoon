26bd5f2f81a0a6d4317ee33988e0c1bad5e69c00
Lee Richards <lr337@sussex.ac.uk>
2020-04-17 16:57:38 +0100

    Added Winner Pop up Screen

---
+++ src/main/java/com/watson/propert/tycoon/control/PtWinnerCtrl.java
@@ -0,0 +1,29 @@
+package com.watson.propert.tycoon.control;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class PtWinnerCtrl {
+
+    @FXML
+    private Label WINNING_PLAYER;
+
+    @FXML
+    private Button BTN_NEW;
+
+    @FXML
+    private Button BTN_QUIT;
+
+    @FXML
+    void newGame(ActionEvent event) {
+
+    }
+
+    @FXML
+    void quitGame(ActionEvent event) {
+
+    }
+
+}
